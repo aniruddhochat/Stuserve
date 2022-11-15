56417761e80d93c2577da6fd2f8f154721eaa7a4
Garrett Spencer <spencegj@iu.edu>
2022-11-15 13:25:12 -0500

    Added CORS

--- app.js
+++ app.js
@@ -17,6 +17,16 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
+app.use((_, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*'); // or 'localhost:8888'
+  res.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.set(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  return next();
+}); // sets headers before routes
+
 // Route Imports
 const service = require("./routes/serviceRoute");
 const user = require("./routes/userRoute");
