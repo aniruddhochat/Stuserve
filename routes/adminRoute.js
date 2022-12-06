b30057b22dad80c4f34b1359004e1d92e32f7f97
Choksi, Mudit Devenkumar <mchoksi@iu.edu>
2022-12-05 21:57:40 -0500

    Admin created

---
+++ routes/adminRoute.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const {
+    getAllAdminProviders,
+    adminDeleteProvider,
+    adminGetAllServicesApproval,
+    adminDeleteService,
+    approveProvider,
+    approveService,
+} = require("../controllers/providerController");
+const {
+  isAuthenticatedProvider,
+  authorizeRoles,
+} = require("../middleware/auth");
+
+const router = express.Router();
+
+router.route("/getAllAdminProviders").get(getAllAdminProviders);
+
+router.route("/adminDeleteProvider").post(adminDeleteProvider);
+
+router.route("/adminGetAllServicesApproval").get(adminGetAllServicesApproval);
+
+router.route("/adminDeleteService").post(adminDeleteService);
+
+router.route("/approveProvider").post(approveProvider);
+
+router.route("/approveService").post(approveService);
+
+
+// Unauthorized view providers (hides important provider info like password)
+router.route("/providerdetails/:id").get(getProviderDetails);
+router.route("/providerdetails").get(getAllProviderDetails);
+
+router
+  .route("/password/updateprovider")
+  .put(isAuthenticatedProvider, updatePassword);
+
+router.route("/me/updateprovider").put(isAuthenticatedProvider, updateProfile);
+
+router
+  .route("/admin/provider")
+  .get(isAuthenticatedProvider, authorizeRoles("admin"), getAllProvider);
+
+router
+  .route("/admin/provider/:id")
+  .get(isAuthenticatedProvider, authorizeRoles("admin"), getSingleProvider)
+  .delete(isAuthenticatedProvider, authorizeRoles("admin"), deleteProvider);
+
+module.exports = router;
